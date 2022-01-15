4dc88e10aac99fa258726f6419441b7916db83ee
explosion-mental <explosion0mental@gmail.com>
2022-01-15 15:56:34 -0500

    compiler optimization to level 3, which removed almost 2 seconds of execution time (insane..)

--- config.mk
+++ config.mk
@@ -13,7 +13,7 @@ LIBS = -lm
 
 # flags
 CPPFLAGS = -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DSTB_IMAGE_IMPLEMENTATION -DVERSION=\"${VERSION}\"
-CFLAGS   = -g -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -g -std=c99 -pedantic -Wall -O3 ${INCS} ${CPPFLAGS}
 LDFLAGS  = -g ${LIBS}
 #CFLAGS  = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
 #LDFLAGS = ${LIBS}
